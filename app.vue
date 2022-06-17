58301a7258c2ea673ff21264fabdd57c0c0a6488
seita1996 <taha.seita@gmail.com>
2022-06-17 14:52:37 +0900

    card layout

--- app.vue
+++ app.vue
@@ -1,19 +1,32 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <!-- <NuxtWelcome /> -->
-    <div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-      <div>
-        <span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
-          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
-        </span>
+  <div class="bg-gray-200 h-screen">
+    <div class="flex flex-row">
+      <div class="basis-1/2 h-40 bg-white rounded-lg border m-3">日時</div>
+      <div class="basis-1/2 h-40 bg-white rounded-lg border m-3">天気</div>
+    </div>
+    <div class="flex flex-row">
+      <div class="basis-1/3 h-1/2 m-3">
+        <card-list :title="'今日の予定'" />
       </div>
-      <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
-      <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-        The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-      </p>
+      <div class="basis-1/3 m-3">
+        <card-list :title="'明日以降の予定'" />
+      </div>
+      <div class="basis-1/3 m-3">
+        <card-list :title="'TODO'" />
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import CardList from '~/components/CardList.vue'
+export default {
+  components: {
+    CardList
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ components/CardList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="basis-1/3 bg-white rounded-lg border">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2 text-gray-700 text-base">{{ title }}</div>
+      <div class="shadow-md border rounded-md text-gray-700 text-base m-3">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-gray-600 text-base">
+            買い物
+          </div>
+          <div class="flex justify-between">
+            <div class="text-2xl text-gray-600 text-base">
+              5/1 15:00 ~ 17:00
+            </div>
+            <img class="inline object-cover w-8 h-8 rounded-full" src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
+          </div>
+        </div>
+      </div>
+      <div class="shadow-md border rounded-md text-gray-700 text-base m-3">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-gray-600 text-base">
+            何かある
+          </div>
+          <div class="text-2xl text-gray-600 text-base">
+            5/1 終日
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'title'
+    },
+  },
+}
+</script>
