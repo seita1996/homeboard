956ea84ccdff358fe568cf5949f4b57d62d02863
seita1996 <taha.seita@gmail.com>
2022-06-18 06:32:24 +0900

    weather forecast layout

--- components/WeatherForecast.vue
+++ components/WeatherForecast.vue
@@ -1,13 +1,58 @@
 <template>
-  <div class="flex justify-around">
-    <div class="w-150 m-3">
-      <img src="~/assets/svg/day.svg" width="150px" height="150px" />
+  <div class="flex justify-around px-6 py-4">
+    <div class="w-180">
+      <div class="flex justify-center text-2xl text-gray-400 text-base">
+        5/1
+      </div>
+      <div class="flex flex-row">
+        <div class="basis-1/2">
+          <img src="~/assets/svg/day.svg" width="180px" height="180px" />
+        </div>
+        <div class="basis-1/2 my-auto">
+          <div class="text-2xl text-gray-400 text-base">
+            25℃/18℃
+          </div>
+          <div class="text-2xl text-gray-400 text-base">
+            0%
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="w-150 m-3">
-      <img src="~/assets/svg/rainy-7.svg" width="150px" height="150px" />
+    <div class="w-180">
+      <div class="flex justify-center text-2xl text-gray-400 text-base">
+        5/2
+      </div>
+      <div class="flex flex-row">
+        <div class="basis-1/2">
+          <img src="~/assets/svg/rainy-7.svg" width="180px" height="180px" />
+        </div>
+        <div class="basis-1/2 my-auto">
+          <div class="text-2xl text-gray-400 text-base">
+            20℃/18℃
+          </div>
+          <div class="text-2xl text-gray-400 text-base">
+            60%
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="w-150 m-3">
-      <img src="~/assets/svg/thunder.svg" width="150px" height="150px" />
+    <div class="w-180">
+      <div class="flex justify-center text-2xl text-gray-400 text-base">
+        5/3
+      </div>
+      <div class="flex flex-row">
+        <div class="basis-1/2">
+          <img src="~/assets/svg/thunder.svg" width="180px" height="180px" />
+        </div>
+        <div class="basis-1/2 my-auto">
+          <div class="text-2xl text-gray-400 text-base">
+            18℃/10℃
+          </div>
+          <div class="text-2xl text-gray-400 text-base">
+            100%
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
