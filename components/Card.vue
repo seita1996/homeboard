a6d0909241aa2fc61b48cead56beb6217b133365
seita1996 <taha.seita@gmail.com>
2022-06-21 20:10:08 +0900

    create card component

---
+++ components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="shadow-md border rounded-md text-gray-700 text-base m-3">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2 text-gray-600 text-base">
+        {{ cardTitle }}
+      </div>
+      <div class="flex justify-between">
+        <div class="text-2xl text-gray-600 text-base">
+          {{ cardTime }}
+        </div>
+        <div class="flex justify-end">
+          <div v-for="cardIconUrl in cardIconUrls">
+            <img class="inline object-cover w-8 h-8 ml-2 rounded-full border shadow-xs" :src="cardIconUrl" alt="Profile image"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  props: {
+    cardIconUrls: {
+      type: Array,
+      default: [
+        'https://3.bp.blogspot.com/-p1j5JG0kN8I/Wn1ZUJ3CbuI/AAAAAAABKK4/hKPhQjTXXv0o3QXh1J0rQ4TaFqGqUGu7ACLcBGAs/s800/animal_smile_kuma.png',
+        'https://2.bp.blogspot.com/-mRJKwyORJkQ/Wn1ZTOBrszI/AAAAAAABKKs/Bg5yjLL9RYwmfUM0pEkBA3Ky3ui0IOZWQCLcBGAs/s800/animal_smile_inu.png'
+      ]
+    },
+    cardTime: {
+      type: String,
+      default: 'cardTime'
+    },
+    cardTitle: {
+      type: String,
+      default: 'cardTitle'
+    },
+  },
+}
+</script>
--- components/CardList.vue
+++ components/CardList.vue
@@ -2,35 +2,20 @@
   <div class="basis-1/3 bg-white rounded-lg border">
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2 text-gray-700 text-base">{{ title }}</div>
-      <div class="shadow-md border rounded-md text-gray-700 text-base m-3">
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2 text-gray-600 text-base">
-            買い物
-          </div>
-          <div class="flex justify-between">
-            <div class="text-2xl text-gray-600 text-base">
-              5/1 15:00 ~ 17:00
-            </div>
-            <img class="inline object-cover w-8 h-8 rounded-full" src="https://github.com/seita1996.png" alt="Profile image"/>
-          </div>
-        </div>
-      </div>
-      <div class="shadow-md border rounded-md text-gray-700 text-base m-3">
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2 text-gray-600 text-base">
-            何かある
-          </div>
-          <div class="text-2xl text-gray-600 text-base">
-            5/1 終日
-          </div>
-        </div>
-      </div>
+      <card :cardTitle="'買い物'" :cardTime="'5/1 10:00 ~ 11:00'" />
+      <card :cardTitle="'病院'" :cardTime="'5/1 15:00 ~ 17:00'" :cardIconUrls="['https://github.com/seita1996.png']" />
+      <card :cardTitle="'薬を飲む'" :cardTime="'5/1 終日'" :cardIconUrls="[]" />
     </div>
   </div>
 </template>
 
 <script>
+import Card from './Card.vue'
+
 export default {
+  components: {
+    Card,
+  },
   data () {
     return {
     }
