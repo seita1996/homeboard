abccdc5b76f71bbd5c3f417d1b3099d2aa42b947
seita1996 <taha.seita@gmail.com>
2022-11-26 06:07:50 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default {
-  ssr: false,
+  // ssr: false,
   buildModules: ['@nuxtjs/tailwindcss'],
   modules: ['@nuxtjs/tailwindcss'],
   build: {
