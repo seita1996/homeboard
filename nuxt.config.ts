71b81bba243399cfc394ab2ed0d4ac87a0b3cf4d
seita1996 <taha.seita@gmail.com>
2023-02-22 09:39:19 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default {
+export default defineNuxtConfig({
   // ssr: false,
-  buildModules: ['@nuxtjs/tailwindcss'],
   modules: ['@nuxtjs/tailwindcss'],
   build: {
     transpile: ['@heroicons/vue']
-  },
-  external: ['vue']
-}
+  }
+})
